0a436d81a053bf280646c76a3f50b25b6ab80fce
Oliver Kasten <oliver.kasten@hslu.ch>
2025-01-02 00:13:15 +0100

    addded directory testdata with an assembly language testfiles

---
+++ testdata/for_i_1_to_10.asm
@@ -0,0 +1,12 @@
+; =================================================
+; for(int i=1; i<10; i++)
+; =================================================
+  	LD R0, 1 		; R0 = 1;
+.start_loop			; for(; R0<=10; R0++)
+  	CMP R0, 10		; done if Z or: (R0-10>0)
+  	BRP $end_loop	; if Pos, jump to end_loop
+  	; do something
+  	ADD R0, 1		; R0++
+  	BRA $start_loop	; jump to beginning of loop
+.end_loop
+  	HALT
